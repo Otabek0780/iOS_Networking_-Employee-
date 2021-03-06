d0aebd70f0b025ff3dfb3e97cb04a62270090e56
Otabek0780 <mrdunacdtm@gmail.com>
2021-03-06 13:12:52 +0900

    Initial Commit

---
+++ iOS_Networking_(Employee)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_Networking_(Employee)
+//
+//  Created by kim jong moon on 06/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_Networking_(Employee)Tests/iOS_Networking__Employee_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_Networking__Employee_Tests.swift
+//  iOS_Networking_(Employee)Tests
+//
+//  Created by kim jong moon on 06/03/2021.
+//
+
+import XCTest
+@testable import iOS_Networking__Employee_
+
+class iOS_Networking__Employee_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
